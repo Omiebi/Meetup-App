a9f50f779fd5eaa69d197f6c79e9a536885c16b2
Omiebi <tamunomiebiwilliams@gmail.com>
2022-07-17 23:42:34 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Todo from "./components/Meetup";
+
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master react" />
+      <Todo text="Explore the full react course" />
+      
+    </div>
+  );
+}
+
+export default App;
